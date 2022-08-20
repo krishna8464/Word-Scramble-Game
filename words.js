6044135ac43ee0d768c15da6749ea032cc9edc2c
YENDA VAMSHI KRISHNA <82109628+krishna8464@users.noreply.github.com>
2022-08-20 14:06:37 +0530

    added initial play backs for the game

---
+++ words.js
@@ -0,0 +1,94 @@
+const words = [
+    {
+        word: "sensex",
+        hint: "The word related to stock exchange"
+    },
+    {
+        word: 'number',
+        hint: 'Math symbol used for counting'
+    },
+    {
+        word: 'exchange',
+        hint: 'The act of trading'
+    },
+    {
+        word: "canvas",
+        hint: 'Piece of fabric for oil painting'
+    },
+    {
+        word: 'garden',
+        hint: 'Space for planting flowers and plants'
+    },
+    {
+        word: 'position',
+        hint: 'Location of someone or something'
+    },
+    {
+        word: 'feather',
+        hint: 'Hair like outer covering of bird'
+    },
+    {
+        word: 'comfort',
+        hint: 'A pleasant feeling of relaxation'
+    },
+    {
+        word: 'tongue',
+        hint: 'The muscular organ of mouth'
+    },
+    {
+        word: 'expansion',
+        hint: 'The process of increse or grow'
+    },
+    {
+        word: 'country',
+        hint: 'A politically identified region'
+    },
+    {
+        word: 'group',
+        hint: 'A number of objects are persons'
+    },
+    {
+        word: 'taste',
+        hint: 'Ability of tongue to detect flavour'
+    },
+    {
+        word: 'store',
+        hint: 'Large shop where goods are stored'
+    },
+    {
+        word: 'field',
+        hint: 'Area of land for farming activities'
+    },
+    {
+        word: 'friend',
+        hint: 'Person other then a family member'
+    },
+    {
+        word: 'pocket',
+        hint: 'A bag for carrying small iteams'
+    },
+    {
+        word: 'needle',
+        hint: 'A thin and sharp metal pin'
+    },
+    {
+        word: 'expert',
+        hint: 'Person with extensive knowledge'
+    },
+    {
+        word: 'statement',
+        hint: 'A declaration of something'
+    },
+    {
+        word: 'second',
+        hint: 'One-sixtieth of a minute'
+    },
+    {
+        word: 'library',
+        hint: 'Place contaiaing collection of books'
+    },
+    {
+        word: "meeting",
+        hint: 'Event in which people come together'
+    }
+]
