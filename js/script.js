0ada210887fb57e74122b241979bac566f66baa4
krishna8464 <yendavamshikrishna123@gmail.com>
2023-07-13 18:27:51 +0530

    count updated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+document.getElementById("tectbox").focus();
 const wordText = document.querySelector(".word"),
 hintText = document.querySelector(".hint span"),
 timeText = document.querySelector(".time b"),
@@ -6,6 +7,7 @@ refreshBtn = document.querySelector(".refresh-word"),
 checkBtn = document.querySelector(".check-word");
 let score = document.getElementById("score");
 let scr = 0;
+let wordlenght 
 
 let correctWord, timer;
 
@@ -17,12 +19,16 @@ const initTimer = maxTime => {
            return timeText.innerText = maxTime;
         }
         clearInterval(timer);
-        alert (`Time Out! ${correctWord.toUpperCase()} was the correct word`)
+        alert (`Time Out! ${correctWord.toUpperCase()} was the correct word`);
+        scr=0
+        score.innerText=`${0}`
+        document.getElementById("tectbox").focus();
         initGame(); // calling initGame function , so the game restarts
 }, 1000);
 }
 
 const initGame = () =>{
+    document.getElementById("tectbox").focus();
     initTimer(31); //calling initTimer function with passing with 30 as maxTime value
     let randomObj = words[Math.floor(Math.random() *words.length)]; //Getting random objects from words
     let wordArray = randomObj.word.split(""); // splitting each letter of random words
@@ -31,6 +37,8 @@ const initGame = () =>{
         [wordArray[i],wordArray[j]] = [wordArray[j],wordArray[i]];  //shuffling and swiping wordArray letters randomly
     }
     wordText.innerText = wordArray.join(""); // passing shuffled words as a text
+    console.log(wordArray.length);
+    wordlenght=wordArray.length
     hintText.innerText = randomObj.hint; // passing random object hint as hint text
     correctWord = randomObj.word.toLocaleLowerCase(); //passing random words to correctWord
     inputField.value="";// making input field empty
@@ -39,6 +47,7 @@ const initGame = () =>{
 }
 initGame();
 const checkWord = () => {
+    document.getElementById("tectbox").focus();
     let userWord = inputField.value.toLocaleLowerCase(); // getting user value
     if(!userWord)return alert ("Please enter a word check"); // if user didn't enter any thing
     //if user word doesn't matches with the correct word
@@ -58,4 +67,15 @@ const checkWord = () => {
 refreshBtn.addEventListener("click", initGame);
 checkBtn.addEventListener("click", checkWord);  
 
+
+let inputbox = document.getElementById("tectbox");
+console.log(inputbox.value)
+
+document.addEventListener("keydown", function(event) {
+    console.log(inputbox.value.length)
+    if (inputbox.value.length === wordlenght) {
+        document.getElementById("btn1").click();
+      }
+    
+});
 
